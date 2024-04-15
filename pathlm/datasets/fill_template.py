aeee9012308ece70b3c4dc89d3e365113e543d41
Simone Cocco <coccosimone@proton.me>
2024-04-15 18:21:32 +0800

    Filled template spostato qua da HLE-2024

---
+++ pathlm/datasets/fill_template.py
@@ -0,0 +1,98 @@
+from argparse import ArgumentParser
+from random import choice
+from pathlm.utils import get_data_template_dir, get_filled_templates_dir, get_raw_paths_dir
+from os.path import join, exists
+from re import compile, finditer
+
+TAGS = {
+    '<pi>': 'pi',
+    '<rp>': 'rp',
+    '<shared entity>': 'se',
+    '<type of entity>': 'te',
+    '<relation>': 're'
+}
+
+def fill_template(template_row_and_tags: list[tuple], path: str) -> str:
+    template_row: list = list(template_row_and_tags[0])
+    tags: tuple[str] = template_row_and_tags[1]
+    for tag in tags:
+        template_row[tag[1]] = f'<start_{TAGS[tag[0]]}> {path[tag[0]]} <end_{TAGS[tag[0]]}>'
+        template_row[tag[1]+1:tag[2]] = [''] * (tag[2] - tag[1] - 1)
+
+    return ' '.join(template_row)
+
+def get_type_of_entity(entity: str) -> str:
+    return 'USER' if entity[0] == 'U' else 'ENTITY' # TODO
+
+def explode_relation(relation_text: str) -> dict:
+    return {
+        '<pi>': relation_text[2],
+        '<rp>': relation_text[-1],
+        '<shared entity>': relation_text[-3],
+        '<type of entity>': get_type_of_entity(relation_text[-3]),
+        '<relation>': relation_text[-2]
+    }
+
+def preprocess_template(template_rows: list) -> list:
+    pattern = compile(r'(<pi>|<rp>|<shared entity>|<type of entity>|<relation>){1,}')
+    template_preprocessed_rows: list = []
+    for i, row in enumerate(template_rows):
+        row_tags: list = []
+        print(f'Processing row {i+1} of {len(template_rows)}')
+        for match in finditer(pattern, row):
+            row_tags.append((match.group(), match.start(), match.end()))
+        template_preprocessed_rows.append((row, row_tags))
+
+    return template_preprocessed_rows
+
+if __name__ == '__main__':
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('--blank-template-file-path', '-iT', type=str, default='template.txt', required=True,
+                        help='Path to the blank template file')
+    parser.add_argument('--raw-paths-file-path', '-iP', type=str, required=True,
+                        help='Path to the raw paths file')
+    parser.add_argument('--filled-template-file-path', '-oT', type=str, default='filled_template.txt',
+                        help='Path to the filled template file')
+    parser.add_argument('--dataset-name', '-d', type=str, default='ml1m',
+                        help='Name of the dataset')
+    parser.add_argument('--max-path', '-L', type=int, default=100,
+                        help='Maximum number of path used to fill the template')
+    args = parser.parse_args()
+
+    template_dir_path: str = get_data_template_dir(args.dataset_name)
+    blank_template_file_path: str = join(template_dir_path, args.blank_template_file_path)
+
+    if not exists(blank_template_file_path):
+        print(f'Error: {blank_template_file_path} does not exist')
+        exit(1)
+
+    with open(blank_template_file_path) as template_file:
+        blank_template_rows: list = template_file.readlines()
+
+    preprocessed_template_rows: list = preprocess_template(blank_template_rows)
+    raw_paths_dir_path: str = get_raw_paths_dir(args.dataset_name)
+    raw_paths_file_path: str = join(raw_paths_dir_path, args.raw_paths_file_path)
+
+    if not exists(raw_paths_file_path):
+        print(f'Error: {raw_paths_file_path} does not exist')
+        exit(1)
+
+    with open(raw_paths_file_path) as raw_paths_file:
+        raw_paths_rows: list = raw_paths_file.readlines()
+
+    processed_paths_rows: list[tuple] = [
+        (explode_relation(exploded_relation := row.strip().split(' ')), ' '.join(row[:2]+[row[-1]]))
+        for row in raw_paths_rows[:args.max_path]
+    ]
+
+    filled_template_rows: list = [
+        (fill_template(choice(preprocessed_template_rows), row[0]), row[1])
+        for row in processed_paths_rows
+    ]
+    
+    filled_template_file_path: str = join(get_filled_templates_dir(args.dataset_name), args.filled_template_file_path)
+    with open(filled_template_file_path, 'w') as filled_template_file_obj:
+        filled_template_file_obj.writelines([
+            f'<start_rec> {line[1].strip()} <end_rec> <start_exp> {line[0].strip()[1:-1]} <end_exp>'
+            for line in filled_template_rows
+        ])
