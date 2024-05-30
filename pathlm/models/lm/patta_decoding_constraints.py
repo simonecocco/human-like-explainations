b0e7596f7ed7ad114f574f5ae532eda9f04d7b59
Simone Cocco <coccosimone@proton.me>
2024-05-30 10:07:55 +0200

    Modificato il logits processor

--- pathlm/models/lm/patta_decoding_constraints.py
+++ pathlm/models/lm/patta_decoding_constraints.py
@@ -1,7 +1,7 @@
-from sympy import expand
+from html import entities
 from transformers import LogitsProcessor
 from string import ascii_uppercase
-import math
+from math import exp, inf
 import torch
 from pathlm.datasets import fill_template
 from pathlm.models.lm.tokenize_dataset import expand_patta_special_token
@@ -12,16 +12,87 @@ from pathlm.utils import Cache
 from pathlm.datasets.fill_template import fill_entities_ids, get_type_of_entity
 from json import load
 
-# https://huggingface.co/docs/transformers/v4.40.1/en/internal/generation_utils#transformers.LogitsProcessor
-class PattaLogitsProcessor(LogitsProcessor):
+class PenalityLogitsProcessor(LogitsProcessor):
     __slots__: list[str] = [
-        'special_token_behavior',
-        'banned_token_ids'
+        'penality_dict',
     ]
 
-    def __init__(self, max_len: int, tokenizer, dataset_name, **kwargs):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.penality_dict: dict = {}
+
+    def update_scores(self, scores: torch.FloatTensor) -> torch.FloatTensor:
+        for (index, val) in self.penality_dict.items():
+            scores[:, index] -= exp(val)
+
+        return scores
+
+    def update_penality(self, input_ids: torch.LongTensor) -> None:
+        last_id: int = int(input_ids[-1])
+        self.penality_dict[last_id] = self.penality_dict.get(last_id, 1) + 1
+
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        self.update_penality(input_ids)
+        scores = self.update_scores(scores)
+        return scores
+
+class BannedTokensLogitsProcessor(LogitsProcessor):
+    __slots__: list[str] = [
+        'tokenizer',
+        'blacklist_token_map'
+    ]
+
+    def __init__(self, tokenizer, **kwargs):
+        self.tokenizer = tokenizer
+        self.blacklist_token_map: list = []
+
+    def add_token(self, token: str|int):
+        if type(token) == str:
+            token = self.tokenizer.convert_tokens_to_ids(token)
+        self.blacklist_token_map.append(token)
+
+    def remove_token(self, token: str|int):
+        if type(token) == str:
+            token = self.tokenizer.convert_tokens_to_ids(token)
+        del self.blacklist_token_map[self.blacklist_token_map.index(token)]
+
+    def ban_tokens(self, scores: torch.FloatTensor) -> torch.FloatTensor:
+        for token in self.blacklist_token_map:
+            scores[:, token] = -inf
+        return scores
+        
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        return self.ban_tokens(scores)
+
+
+class PattaLogitsProcessor(LogitsProcessor):
+    __slots__: list[str] = [
+        'persistent_cache_storage',
+        'cache_dict'
+    ]
+
+    def load_big_data(self, data_name)
+
+    def __init__(self, max_len: int, tokenizer, dataset_name, banned_tokens_lp, **kwargs):
         super().__init__(**kwargs)
         
+        self.persistent_cache_storage: Cache = Cache()
+        self.cache_dict: dict = {}
+
+        entities_ids_name = f'entities_{dataset_name}'
+        if not (entities_ids_name in self.persistent_cache_storage):
+            entities_ids = fill_entities_ids(dataset_name)
+            
+            self.cache_dict[entities_ids_name] = entities_ids
+            self.persistent_cache_storage[entities_ids_name] = entities_ids
+        else:
+            entities_ids = self.persistent_cache_storage[entities_ids_name]
+            self.cache_dict[entities_ids_name] = entities_ids
+
+        
+
         self.entities_ids = fill_entities_ids(dataset_name)
         self.special_already_used_tokens: list[int] = [tokenizer.convert_tokens_to_ids('<end_exp>')]
         self.max_output_len: int = max_len
@@ -46,9 +117,6 @@ class PattaLogitsProcessor(LogitsProcessor):
         self.end_special_tokens: list[int] = self.tokenizer_obj.convert_tokens_to_ids(expand_patta_special_token(with_prefix=['end']))
         self.close_token: int = -1
 
-    def edit_logits(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
-        last_token_id: int = int(input_ids[0][-1])
-        return scores
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
-        return self.edit_logits(input_ids, scores)
+        return scores
